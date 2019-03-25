1076429c57f9c3282fcbd759c6c091d1a41b0b03
John Goure <johngoure@protonmail.com>
2019-03-25 17:47:39 -0500

    Changed the resource search around. Creeps look in storage before they look on the ground.

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,38 +1,40 @@
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
-	    }
-	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.upgrading = true;
-	    }
+        }
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+        }
 
-	    if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {});
             }
-        }
-        else {
-            const targets = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-            if (targets) {
-                creep.moveTo(targets);
-                creep.pickup(targets);
-            }else {
-          const buildings = creep.room.find(FIND_STRUCTURES, {
-          filter: (structure) => {
-            return (structure.structureType == structure.structureType == STRUCTURE_CONTAINER) &&
-              structure.energy > 50;
-          }});
-                
-                if(creep.withdraw(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        } else {
+            const buildings = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.energy > 50;
+                }
+            });
+            if (buildings.length > 0) {
+                if (creep.withdraw(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(buildings[0]);
+                }
+
+            } else {
+                const targets = creep.room.find(FIND_DROPPED_RESOURCES);
+                if (targets.length) {
+                    creep.moveTo(targets[0], {});
+                    creep.pickup(targets[0])
                 }
             }
         }
-	}
+    }
 };
 
 module.exports = roleUpgrader;
--- role.worker.js
+++ role.worker.js
@@ -1,39 +1,40 @@
 let roleWorker = {
   run: function (creep) {
-     if(creep.memory.building && creep.carry.energy == 0) {
-         creep.memory.building = false;
-	 }
-	 if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	     creep.memory.building = true;
-	 }
+    if (creep.memory.building && creep.carry.energy == 0) {
+      creep.memory.building = false;
+    }
+    if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.building = true;
+    }
 
     if (creep.memory.building == false) {
-      const targets = creep.room.find(FIND_DROPPED_RESOURCES);
-      if (targets.length) {
-        creep.moveTo(targets[0], {
-        });
-        creep.pickup(targets[0])
-      }else {
-          const buildings = creep.room.find(FIND_STRUCTURES, {
-          filter: (structure) => {
+      const buildings = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
             return (structure.structureType == structure.structureType == STRUCTURE_CONTAINER) &&
-              structure.energy > 50;
-          }});
-                
-                if(creep.withdraw(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(buildings[0]);
-                }
-            } 
-    } 
-    else {
-        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
+                structure.energy > 50;
+        }
+    });
+    if (buildings.length > 0) {
+        if (creep.withdraw(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(buildings[0]);
+        }
+
+    } else {
+        const targets = creep.room.find(FIND_DROPPED_RESOURCES);
+        if (targets.length) {
+            creep.moveTo(targets[0], {});
+            creep.pickup(targets[0])
+        }
+    }
+    } else {
+      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+      if (targets.length) {
+        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0]);
+        }
       }
     }
-  };
+  }
+};
 
-  module.exports = roleWorker;
+module.exports = roleWorker;
