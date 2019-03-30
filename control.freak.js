dc241f829ed13734c95d83eecf55d12ae273af96
John Goure <johngoure@protonmail.com>
2019-03-30 13:24:46 -0500

    Updated control freak.

--- control.freak.js
+++ control.freak.js
@@ -4,30 +4,35 @@ let roleDigger = require('digger');
 let roleScrumMaster = require('scrum.master');
 let roleDefender = require('defender');
 let roleRepairMan = require('repair.squad');
+let roleTransporter = require('transporter');
 
 let controlFreak = {
-    run: function() {
-        for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'digger1' || creep.memory.role == 'digger2') {
-            roleDigger.run(creep);
+    run: function () {
+        for (var name in Game.creeps) {
+            var creep = Game.creeps[name];
+            if (creep.memory.role == 'digger1' || creep.memory.role == 'digger2') {
+                roleDigger.run(creep);
+            }
+            if (creep.memory.role == 'worker') {
+                roleWorker.run(creep);
+            }
+            if (creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
+            }
+            if (creep.memory.role == 'scrum_master') {
+                roleScrumMaster.run(creep);
+            }
+            if (creep.memory.role == 'transporter') {
+                roleTransporter.run(creep);
+            }
+            if (creep.memory.role == 'attacker') {
+                roleDefender.run(creep);
+            }
+            if (creep.memory.role == 'repair_squad') {
+
+                roleRepairMan.run(creep);
+            }
         }
-        if(creep.memory.role == 'worker') {
-            roleWorker.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'scrum_master') {
-            roleScrumMaster.run(creep);
-        }
-        if(creep.memory.role == 'attacker') {
-            roleDefender.run(creep);
-        }
-        if(creep.memory.role == 'repair_squad') {
-            roleRepairMan.run(creep);
-        }
-    }
     }
 }
 
