58a4b343898bc5cf8f224778d4ef118190cfa59f
John Goure <johngoure@protonmail.com>
2019-03-30 13:19:49 -0500

    Updated the worker to focus on making new structures or filling energy in the tower.

--- role.worker.js
+++ role.worker.js
@@ -1,36 +1,51 @@
+let Transport = require('transporter');
+
 let roleWorker = {
   run: function (creep) {
-    if (creep.memory.building && creep.carry.energy == 0) {
-      creep.memory.building = false;
+    if (creep.memory.working && creep.carry.energy == 0) {
+      creep.memory.working = false;
     }
-    if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.building = true;
+    if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.working = true;
     }
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-    if (creep.memory.building == false) {
+    if (creep.memory.working == false) {
       const buildings = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER) &&
-                            structure.energy > 50;
-                    }
-                });
-                if (buildings.length > 0) {
-                    if (creep.withdraw(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(buildings[0]);
-                    }
+        filter: (structure) => {
+          return ((structure.id != '5c9a24e5cac6ca1076c66c3f' || structure.id != '5c9963dcc9db991063d82dfe') && (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) &&
+            structure.store[RESOURCE_ENERGY] > 0)
+        }
+      });
+      let closest = creep.pos.findClosestByPath(buildings);
+      if (closest) {
+        if (creep.withdraw(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(closest);
+        }
 
-                } else {
-                    const targets = creep.room.find(FIND_DROPPED_RESOURCES);
-                    if (targets.length) {
-                        creep.moveTo(targets[0], {});
-                        creep.pickup(targets[0])
-                    }
-                }
+      } else {
+        const resource = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+        if (resource) {
+          creep.moveTo(resource);
+          creep.pickup(resource);
+        }
+      }
+    } else if (targets.length > 0) {
+      creep.memory.working = true;
+      if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(targets[0]);
+      }
     } else {
-      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-      if (targets.length) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0]);
+      creep.memory.working = true;
+      const buildings = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_TOWER) &&
+            structure.energy < structure.energyCapacity;
+        }
+      });
+      if (buildings.length > 0) {
+        if (creep.transfer(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(buildings[0], {});
         }
       }
     }
