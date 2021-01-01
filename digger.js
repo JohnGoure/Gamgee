f4cb38dc8f154f576c5de3fd2058607ab1f80244
JohnGoure <johnsgithub@gmail.com>
2020-12-31 18:28:23 -0600

    Cleaned up unused code and strucutre.

--- digger.js
+++ digger.js
@@ -1,32 +1,30 @@
 let digger = {
     run: function (creep) {
         if (creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
             let source = Game.getObjectById(creep.memory.assignedSource);
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
 
-        } else {
-                const buildings = creep.room.find(FIND_STRUCTURES, {
+        } 
+        else {
+            const buildings = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER)
+                    return (structure.structureType == STRUCTURE_CONTAINER);
                 }
             });
-                if (buildings.length > 0) {
-                    let storage = creep.pos.findClosestByPath(buildings)
-                    console.log(storage)
-                    if (storage) {
-                        console.log(storage)
-                        creep.transfer(storage, RESOURCE_ENERGY, creep.carryCapacity); 
-                    }
+            if (buildings.length > 0) {
+                let storage = creep.pos.findClosestByPath(buildings);
+                if (storage) {
+                    creep.transfer(storage, RESOURCE_ENERGY, creep.carryCapacity); 
+                }
                     
-                    else if (storage.store[RESOURCE_ENERGY] >= storage.storeCapacity - 44) {
-                        creep.drop(RESOURCE_ENERGY, creep.carryCapacity)
-                    }
-                } else {
+                else if (storage.store[RESOURCE_ENERGY] >= storage.storeCapacity - 44) {
                     creep.drop(RESOURCE_ENERGY, creep.carryCapacity);
                 }
+            } else {
+                creep.drop(RESOURCE_ENERGY, creep.carryCapacity);
+            }
         }
     }
 }
