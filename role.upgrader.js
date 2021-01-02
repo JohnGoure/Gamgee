2d2bc239bbb52dc69d75f697fe9c6351a051338a
JohnG <johnsgithub@gmail.com>
2021-01-02 12:30:20 -0600

    Upgraders will take on the role of digger or scrum master if there arent any.

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,41 +1,55 @@
+let ScrumMaster = require("scrum.master");
+let Digger = require("digger");
+
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-
-        if (creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
+        let scrumMasters = _.filter(Game.creeps, (creep) => creep.memory.role == "scrum_master");
+        let diggers = _.filter(Game.creeps, (creep) => creep.memory.role == 'digger');
+        if (diggers == 0) {
+            Digger.run(creep);
         }
-        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.upgrading = true;
-        }
-
-        if (creep.memory.upgrading) {
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {});
+        else if (scrumMasters.length == 0) {
+            ScrumMaster.run(creep);
+        } 
+        else {
+            if (creep.memory.upgrading && creep.carry.energy == 0) {
+                creep.memory.upgrading = false;
             }
-        } else {
-            const buildings = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER && structure.id != "5c9a24e5cac6ca1076c66c3f" || structure.structureType == STRUCTURE_STORAGE) &&
-                        structure.store[RESOURCE_ENERGY] > 0 && structure.id != "5c9a24e5cac6ca1076c66c3f"
-                }
-            });
-            let closest = creep.pos.findClosestByPath(buildings);
-            if (closest) {
-                if (creep.withdraw(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(closest);
-                }
-
-            } else {
-                const targets = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-                if (targets) {
-                    creep.moveTo(targets);
-                    creep.pickup(targets)
-                }
+            if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+                creep.memory.upgrading = true;
             }
-
+    
+            if (creep.memory.upgrading) {
+                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {});
+                }
+            } 
+            else {
+                const buildings = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER && structure.id != "5c9a24e5cac6ca1076c66c3f" || structure.structureType == STRUCTURE_STORAGE) &&
+                            structure.store[RESOURCE_ENERGY] > 0 && structure.id != "5c9a24e5cac6ca1076c66c3f"
+                    }
+                });
+                let closest = creep.pos.findClosestByPath(buildings);
+                if (closest) {
+                    if (creep.withdraw(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(closest);
+                    }
+    
+                } else {
+                    const targets = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+                    if (targets) {
+                        creep.moveTo(targets);
+                        creep.pickup(targets)
+                    }
+                }
+    
+            }
         }
+        
     }
 };
 
