72fd8f2d8bfd0a51e0a23a898aa224cbef7b5bb3
JohnG <johnsgithub@gmail.com>
2021-01-02 12:31:29 -0600

    Diggers will be assigned a source if they don't have one. Diggers also drop resources when storage is full.

--- digger.js
+++ digger.js
@@ -2,6 +2,20 @@ let digger = {
     run: function (creep) {
         if (creep.carry.energy < creep.carryCapacity) {
             let source = Game.getObjectById(creep.memory.assignedSource);
+            if (source == null) { 
+                for (var spawnName in Game.spawns) {
+                    const spawn = Game.spawns[spawnName];
+                    const energySources = spawn.room.find(FIND_SOURCES);        
+                
+                    for (var energySource in energySources) {
+                        const diggersAtSource = _.filter(Game.creeps, (creep) => creep.memory.assignedSource == energySources[energySource].id).length;
+                        if (diggersAtSource < DIGGERMAXCOUNT) { 
+                            creep.memory.assignedSource = energySources[energySource].id;
+                        }
+                    } 
+                }
+                
+            }
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
@@ -16,17 +30,24 @@ let digger = {
             if (buildings.length > 0) {
                 let storage = creep.pos.findClosestByPath(buildings);
                 if (storage) {
-                    creep.transfer(storage, RESOURCE_ENERGY, creep.carryCapacity); 
+                    if (creep.transfer(storage, RESOURCE_ENERGY, creep.carryCapacity) == ERR_NOT_IN_RANGE || creep.transfer(storage, RESOURCE_ENERGY, creep.carryCapacity) == ERR_FULL) {
+                        DropResources(creep);
+                    } 
                 }
                     
-                else if (storage.store[RESOURCE_ENERGY] >= storage.storeCapacity - 44) {
-                    creep.drop(RESOURCE_ENERGY, creep.carryCapacity);
+                else if (storage.store[RESOURCE_ENERGY] >= storage.storeCapacity - 50) {
+                    DropResources(creep);
                 }
-            } else {
-                creep.drop(RESOURCE_ENERGY, creep.carryCapacity);
+            } 
+            else {
+                DropResources(creep);
             }
         }
     }
+}
+
+function DropResources(creep) {
+    creep.drop(RESOURCE_ENERGY, creep.carryCapacity);
 }
 
 module.exports = digger;
