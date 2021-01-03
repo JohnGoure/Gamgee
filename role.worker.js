fa4de379ec2e1e0aea81c00047d0a8a2d2791bd8
JohnG <johnsgithub@gmail.com>
2021-01-03 11:31:16 -0600

    Added logic that if there is no digger become a digger. If idle become an upgrader.

--- role.worker.js
+++ role.worker.js
@@ -1,55 +1,66 @@
-let Transport = require('transporter');
+let Transporter = require('transporter');
+let Upgrader = require('role.upgrader');
 
 let roleWorker = {
   run: function (creep) {
-    if (creep.memory.working && creep.carry.energy == 0) {
-      creep.memory.working = false;
+    let diggers = _.filter(Game.creeps, (creep) => creep.memory.role == 'digger');
+    if (diggers == 0) {
+        Digger.run(creep);
     }
-    if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
-      creep.memory.working = true;
-    }
-    var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-
-    if (creep.memory.working == false) {
-      const buildings = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return ((structure.id != '5cad579b3b636f1583e1895e' && structure.id != '5cad5a17adfc20719aa12323') && (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) &&
-            structure.store[RESOURCE_ENERGY] > 0)
+    else {
+      if (creep.memory.working && creep.carry.energy == 0) {
+        creep.memory.working = false;
+      }
+      if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+        creep.memory.working = true;
+      }
+      var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+  
+      if (creep.memory.working == false) {
+        const buildings = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => {
+            return ((structure.id != '5cad579b3b636f1583e1895e' && structure.id != '5cad5a17adfc20719aa12323') && (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) &&
+              structure.store[RESOURCE_ENERGY] > 200)
+          }
+        });
+        let closest = creep.pos.findClosestByPath(buildings);
+        if (closest) {
+          if (creep.withdraw(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(closest);
+          }
+  
+        } else {
+          const resource = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+          if (resource) {
+            creep.moveTo(resource);
+            creep.pickup(resource);
+          }
         }
-      });
-      let closest = creep.pos.findClosestByPath(buildings);
-      if (closest) {
-        if (creep.withdraw(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(closest);
+      } else if (target != null) {
+        creep.memory.working = true;
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
         }
-
       } else {
-        const resource = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-        if (resource) {
-          creep.moveTo(resource);
-          creep.pickup(resource);
-        }
-      }
-    } else if (target != null) {
-      creep.memory.working = true;
-      if (creep.build(target) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(target);
-      }
-    } else {
-      creep.memory.working = true;
-      const buildings = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.structureType == STRUCTURE_TOWER) &&
-            structure.energy < structure.energyCapacity;
-        }
-      });
-      if (buildings.length > 0) {
-        if (creep.transfer(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(buildings[0], {});
+        creep.memory.working = true;
+        const buildings = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => {
+            return (structure.structureType == STRUCTURE_TOWER) &&
+              structure.energy < structure.energyCapacity;
+          }
+        });
+        if (buildings.length > 0) {
+          if (creep.transfer(buildings[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(buildings[0], {});
+          }
+        } 
+        else {
+            Upgrader.run(creep);
         }
       }
     }
-  }
+      
+    }
 };
 
 module.exports = roleWorker;
