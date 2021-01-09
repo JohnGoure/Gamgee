9b61a070e2b4a82757d28cf6da479cd97cba7dc2
JohnG <johnsgithub@gmail.com>
2021-01-09 06:46:46 -0600

    Reverted back. Transporters weren't xfering correctly.

--- transporter.js
+++ transporter.js
@@ -49,11 +49,11 @@ function transportEnergyToStorage(creep) {
         creep.memory.upgradeContainerId = containerId;
     }
     if (creep.memory.spawnContainer1Id == null) {
-        const containerId = Game.spawns[creep.memory.spawnName].memory.spawnContainer1Id != null ? Game.spawns[creep.memory.spawnName].memory.spawnContainer1Id.id : null;
+        const containerId = Game.spawns[creep.memory.spawnName].memory.spawnContainer1Id != null ? Game.spawns[creep.memory.spawnName].memory.spawnContainer1Id : null;
         creep.memory.spawnContainer1Id = containerId;
     }
     if (creep.memory.spawnContainer2Id == null) {
-        const containerId = Game.spawns[creep.memory.spawnName].memory.spawnContainer2Id ? Game.spawns[creep.memory.spawnName].memory.spawnContainer2Id.id : null;
+        const containerId = Game.spawns[creep.memory.spawnName].memory.spawnContainer2Id ? Game.spawns[creep.memory.spawnName].memory.spawnContainer2Id : null;
         creep.memory.spawnContainer2Id = containerId;
     }
     const upgradeContainer = Game.getObjectById(creep.memory.upgradeContainerId);
@@ -62,25 +62,26 @@ function transportEnergyToStorage(creep) {
 
     const containers = [upgradeContainer, spawnContainer1, spawnContainer2]
 
-    containers.filter(container => 
-        container != null && container.store[RESOURCE_ENERGY] < container.store.getCapacity());
+    // containers.filter(container => 
+    //     container != null && container.store[RESOURCE_ENERGY] < container.store.getCapacity());
 
-    TransferEnergy(containers[0], creep);
-    // if (spawnContainer1 != null && spawnContainer1.store[RESOURCE_ENERGY] < spawnContainer1.store.getCapacity() * .7) {  
-    //     TransferEnergy(spawnContainer1, creep);
-    // }
-    // else if (spawnContainer2 != null && spawnContainer2.store[RESOURCE_ENERGY] < spawnContainer1.store.getCapacity() *.7) { 
-    //     TransferEnergy(spawnContainer2, creep);
-    // }
-    // else if (upgradeContainer != null && upgradeContainer != 'undefined' && upgradeContainer.store[RESOURCE_ENERGY] < upgradeContainer.store.getCapacity()) { 
-    //     TransferEnergy(upgradeContainer, creep);
-    // }
-    // else if (spawnContainer1 != null && spawnContainer1.store[RESOURCE_ENERGY] < spawnContainer1.store.getCapacity()) {     
-    //     TransferEnergy(spawnContainer1, creep);
-    // }
-    // else if (spawnContainer2 != null && spawnContainer2.store[RESOURCE_ENERGY] < spawnContainer2.store.getCapacity()) {         
-    //     TransferEnergy(spawnContainer2, creep);
-    // }
+    // TransferEnergy(containers[0], creep);
+
+    if (spawnContainer1 != null && spawnContainer1.store[RESOURCE_ENERGY] < spawnContainer1.store.getCapacity() * .7) {  
+        TransferEnergy(spawnContainer1, creep);
+    }
+    else if (spawnContainer2 != null && spawnContainer2.store[RESOURCE_ENERGY] < spawnContainer1.store.getCapacity() *.7) { 
+        TransferEnergy(spawnContainer2, creep);
+    }
+    else if (upgradeContainer != null && upgradeContainer != 'undefined' && upgradeContainer.store[RESOURCE_ENERGY] < upgradeContainer.store.getCapacity()) { 
+        TransferEnergy(upgradeContainer, creep);
+    }
+    else if (spawnContainer1 != null && spawnContainer1.store[RESOURCE_ENERGY] < spawnContainer1.store.getCapacity()) {     
+        TransferEnergy(spawnContainer1, creep);
+    }
+    else if (spawnContainer2 != null && spawnContainer2.store[RESOURCE_ENERGY] < spawnContainer2.store.getCapacity()) {         
+        TransferEnergy(spawnContainer2, creep);
+    }
 
 }
 
@@ -109,6 +110,10 @@ function findFullestContainer(creep) {
         return null;
     }
 
+}
+
+function Print(text) {
+    console.log(text);
 }
 
 module.exports = transporter;
