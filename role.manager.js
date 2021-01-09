5ed0eb4d8badec09ea83b8878befc44de9850316
JohnG <johnsgithub@gmail.com>
2021-01-09 06:25:51 -0600

    Managers now use scrum masters findenergy().

--- role.manager.js
+++ role.manager.js
@@ -1,3 +1,5 @@
+const ScrumMaster = require('scrum.master');
+
 let roleManager = {
     run: function(creep) {
         if (creep.memory.working && creep.carry.energy == 0) {
@@ -6,29 +8,9 @@ let roleManager = {
         if (!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
             creep.memory.working = true;
         }
-        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
         if (creep.memory.working == false) {
-            const buildings = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return ((structure.id != '5cad579b3b636f1583e1895e' || structure.id != '5cad5a17adfc20719aa12323') && (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_CONTAINER) &&
-                  structure.store[RESOURCE_ENERGY] > 0);
-                }
-            });
-            let closest = creep.pos.findClosestByPath(buildings);
-            if (closest) {
-                if (creep.withdraw(closest, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(closest);
-                }
-
-            } 
-            else {
-                const resource = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-                if (resource) {
-                    creep.moveTo(resource);
-                    creep.pickup(resource);
-                }
-            }
+            ScrumMaster.findEnergy(creep);
         }  
         else {
             creep.memory.working = true;
